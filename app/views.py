e44a6fa1574b824dd5f55cf26e7014ff06910f85
Matthew Mahabeer <mattmahabeer@gmail.com>
2019-02-11 03:47:28 +0000

    final commit

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -23,6 +23,19 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile/')
+def profile():
+    """Render profile page."""
+    date=format_date_joined()
+    return render_template('profile.html', date=date)
+@app.route('/profile/')
+def format_date_joined():
+    now = datetime.datetime.now()
+    date_joined = datetime.date(2019, 2, 8)
+    statement = ("Joined " + date_joined.strftime("%B, %Y"))
+    return statement
+    
 
 
 ###
